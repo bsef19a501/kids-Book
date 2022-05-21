77d9b8e104a3e52baadb651f15b597ffe65e1a74
Jawad Akram <bsef19a501@pucit.edu.pk>
2022-05-21 02:58:29 -0700

    kids book task commit 1

---
+++ app/src/main/java/com/example/kidsbook/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.kidsbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.net.Uri;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openRepository(View view) {
+        Uri webpage = Uri.parse("https://github.com/bsef19a501/kids-Book.git");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void alphabets(View view) {
+        Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+        startActivity(intent);
+    }
+}
