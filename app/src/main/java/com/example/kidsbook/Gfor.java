1d0c89fd1c07e007dc28466e74f2bb3dc537591a
Jawad Akram <bsef19a501@pucit.edu.pk>
2022-05-22 12:34:30 -0700

    fifth commit

---
+++ app/src/main/java/com/example/kidsbook/Gfor.java
@@ -0,0 +1,14 @@
+package com.example.kidsbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gfor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gfor);
+    }
+}
